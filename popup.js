f324cc89225548de1eb8b69a6a0398d92238707c
YASH KUMAR <kyash03@student.ubc.ca>
2023-06-19 12:58:04 +0530

    getactivetaburl function

--- popup.js
+++ popup.js
@@ -1,2 +1,11 @@
 let professorRating = 0.0;
 let isTBA;
+
+async function getActiveTabURL() {
+  const tabs = await chrome.tabs.query({
+    currentWindow: true,
+    active: true,
+  });
+
+  return tabs[0];
+}
