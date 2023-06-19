5c3c99aee43eb0c7c92aea9e2188ea3053f62250
YASH KUMAR <kyash03@student.ubc.ca>
2023-06-19 12:58:30 +0530

    hide headers function added

--- popup.js
+++ popup.js
@@ -9,3 +9,14 @@ async function getActiveTabURL() {
 
   return tabs[0];
 }
+
+function hideHeaders() {
+  let sections = document.getElementById("sections");
+  sections.innerHTML = "";
+  sections.style.paddingTop = "20px";
+
+  let h2Elements = document.getElementsByClassName("h2-elements");
+  for (let i = 0; i < h2Elements.length; i++) {
+    h2Elements[i].style.display = "none";
+  }
+}
